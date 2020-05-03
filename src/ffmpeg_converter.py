79fc4fe75940abffbd5c5568a5cc5df609b0a754
Michael Yee <8632035+mkeness@users.noreply.github.com>
2020-05-03 02:42:17 -0700

    Changed to use python libraries instead of ucstom logic to get filename and extension

--- src/ffmpeg_converter.py
+++ src/ffmpeg_converter.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
 
-AVI_EXTENSION = "avi"
-MP4_EXTENSION = "mp4"
+AVI_EXTENSION = ".avi"
+MP4_EXTENSION = ".mp4"
 
 
 def convert_avi_to_mp4(directory_name):
@@ -11,12 +11,14 @@ def convert_avi_to_mp4(directory_name):
     :param directory_name: The directory to convert .avi to .mp4 files
     :return:
     """
-    full_file_names = os.listdir(directory_name)
+    os.chdir(directory_name)
+    full_file_names = [file for file in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(), file))]
+
     for full_file_name in full_file_names:
-        print(full_file_name)
-        file_name_and_extension = full_file_name.split('.')
-        file_name = file_name_and_extension[:len(file_name_and_extension) - 1][0] if len(file_name_and_extension[:len(file_name_and_extension) - 1]) > 0 else ''
-        extension = file_name_and_extension[len(file_name_and_extension) - 1:][0] if len(file_name_and_extension[len(file_name_and_extension) - 1:]) > 0 else ''
+        file_name = os.path.splitext(full_file_name)[0]
+        extension = os.path.splitext(full_file_name)[1]
+
+        print(file_name)
         print(extension)
 
         if file_name == '' or extension == '':
@@ -25,13 +27,10 @@ def convert_avi_to_mp4(directory_name):
 
         if extension == AVI_EXTENSION:
             print("avi extension detected")
-            print(f"{file_name}.{AVI_EXTENSION}")
-            os.chdir(directory_name)
-
-            print(f'{file_name}.{AVI_EXTENSION}')
-            print(f'{file_name}.{MP4_EXTENSION}')
+            print(f'Input File: {file_name}{AVI_EXTENSION}')
+            print(f'Output File: {file_name}{MP4_EXTENSION}')
 
             # Use double quotes around file names in case there are spaces in the file name
-            subprocess.call(f'ffmpeg -ss 00:00:00.0 -i "{file_name}.{AVI_EXTENSION}" -map 0 -b:v 50M -minrate 50M -maxrate 50M -bufsize 100M "{file_name}.{MP4_EXTENSION}"', shell=True)
+            subprocess.call(f'ffmpeg -ss 00:00:00.0 -i "{file_name}{AVI_EXTENSION}" -map 0 -b:v 50M -minrate 50M -maxrate 50M -bufsize 100M "{file_name}{MP4_EXTENSION}"', shell=True)
         else:
             print(f"unknown extension: {extension}")
