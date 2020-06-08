732535b21a51348eec2cb85d4895048b0bb92cb8
Michael Yee <8632035+mkeness@users.noreply.github.com>
2020-06-07 17:49:31 -0700

    Added new mode to convert mp4 to mp3 files

--- src/ffmpeg_converter.py
+++ src/ffmpeg_converter.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 AVI_EXTENSION = ".avi"
+MP3_EXTENSION = ".mp3"
 MP4_EXTENSION = ".mp4"
 
 
@@ -31,6 +32,37 @@ def convert_avi_to_mp4(directory_name):
             print(f'Output File: {file_name}{MP4_EXTENSION}')
 
             # Use double quotes around file names in case there are spaces in the file name
-            subprocess.call(f'ffmpeg -ss 00:00:00.0 -i "{file_name}{AVI_EXTENSION}" -map 0 -b:v 50M -minrate 50M -maxrate 50M -bufsize 100M "{file_name}{MP4_EXTENSION}"', shell=True)
+            subprocess.call(f'ffmpeg -ss 00:00:00.0 -i "{file_name}{AVI_EXTENSION}" \
+                -map 0 -b:v 50M -minrate 50M -maxrate 50M -bufsize 100M \
+                "{file_name}{MP4_EXTENSION}"', shell=True)
         else:
             print(f"unknown extension: {extension}")
+    # TODO: Return number of files converted/processed
+
+
+def convert_mp4_to_mp3(directory_name, file_names):
+    """
+    Convert .mp4 files to .mp3 files
+    :param directory_name: Directory to search for file names (will search through folders)
+    :param file_names: List of strings (file names)
+    :return:
+    """
+    if not directory_name or not file_names:
+        return
+
+    os.chdir(directory_name)
+
+    for root, dirs, files in os.walk(directory_name):
+        for file_name_with_extension in files:
+            file_name_no_extension = os.path.splitext(file_name_with_extension)[0]
+            if file_name_no_extension in file_names or file_name_with_extension in file_names:
+                print(f'Input File: {file_name_no_extension}{MP4_EXTENSION}')
+                print(f'Output File: {file_name_no_extension}{MP3_EXTENSION}')
+                print(os.path.join(root, file_name_with_extension))
+
+                # Use double quotes around file names in case there are spaces in the file name
+                # ffmpeg cmd source: https://askubuntu.com/questions/84584/converting-mp4-to-mp3
+                subprocess.call(f'ffmpeg -i "{file_name_no_extension}{MP4_EXTENSION}" -vn -sn -dn \
+                    -acodec libmp3lame -ac 2 -ab 190k -ar 48000 \
+                    "{file_name_no_extension}{MP3_EXTENSION}"', shell=True)
+    # TODO: Return number of files converted/processed
--- src/ffmpeg_driver.py
+++ src/ffmpeg_driver.py
@@ -1,16 +1,32 @@
 from src import ffmpeg_converter
 
+CONVERT_AVI_TO_MP4_MODE = 'avi to mp4'
+CONVERT_MP4_TO_MP3_MODE = 'mp4 to mp3'
+
 
 def main():
-    # directory to use
-    # 1st usage - convert .avi to .mp4
+    print("Please input the mode you would like to use.")
+    mode = input("Available Modes: 'AVI to MP4', 'MP4 to MP3'\n").lower()
 
-    print("Ensure that ffmpeg.exe is located in the same directory as the raw video files")
-    directory = input("Input directory to convert .avi to .mp4\n")
-    print(directory)
+    print("Ensure that ffmpeg.exe is located in the same directory as the directory inputted")
+    if mode == CONVERT_AVI_TO_MP4_MODE:
+        directory = input("Input directory to convert .avi to .mp4\n")
+        ffmpeg_converter.convert_avi_to_mp4(directory)
+    elif mode == CONVERT_MP4_TO_MP3_MODE:
+        directory = input("Input directory to convert .mp4 to .mp3\n")
 
-    ffmpeg_converter.convert_avi_to_mp4(directory)
+        file_names = []
+        print("Input file names to convert -- Hit enter with nothing when finished")
+        while True:
+            file_name = input()
 
+            if file_name:
+                file_names.append(file_name)
+            else:
+                break
+        ffmpeg_converter.convert_mp4_to_mp3(directory, file_names)
+    else:
+        print("Error, inputted mode not found")
     print("Done")
 
 
